9abfd6d7eae3110c7e300caa471ac116be718219
leeseonju12 <seonju220@gmail.com>
2025-12-09 16:48:58 +0900

    회원 테스트 데이터 3명 생성, 로그인 기능 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,9 +16,10 @@ public class Main {
         System.out.println("==프로그램 시작==");
 
         int lastArticleId = 3;
-        int lastMemberId = 0;
+        int lastMemberId = 3;
 
         makeTestData();
+        makeTestUserData();
 
         while (true) {
             System.out.print("명령어 ) ");
@@ -66,7 +67,29 @@ public class Main {
 
                 System.out.println(id + "번 회원이 가입 되었습니다.");
                 lastMemberId++;
-            } else if (cmd.equals("article write")) {
+            } else if (cmd.equals("login")) {
+                System.out.print("아이디: ");
+                String loginId = sc.nextLine();
+                System.out.print("비밀번호: ");
+                String password = sc.nextLine();
+
+                boolean loginSuccess = false;
+                for (Member member : members) {
+                    if (member.getLoginId().equals(loginId) && member.getPassword().equals(password)) { //현재 검사중인 user객체의 아이디와 비밀번호가 맞는 경우
+                        System.out.println(member.getId() + "번 회원 로그인이 성공되었습니다.");
+                        loginSuccess = true;
+                        break;
+                    }
+                }
+                if (!loginSuccess) {
+                    // 로그인 실패 시 메시지를 한 번만 출력
+                    System.out.println("로그인 실패. 아이디나 비밀번호를 다시 입력하세요.\n");
+                }
+
+
+
+
+        } else if (cmd.equals("article write")) {
                 System.out.println("==게시글 작성==");
                 int id = lastArticleId + 1;
                 System.out.print("제목 : ");
@@ -207,6 +230,17 @@ public class Main {
         articles.add(new Article(2, Util.getNowStr(), Util.getNowStr(), "제목 23", "내용 2"));
         articles.add(new Article(3, Util.getNowStr(), Util.getNowStr(), "제목 1234", "내용 3"));
     }
+    /**
+     * 회원정보 테스트 데이터 생성
+     **/
+    private static void makeTestUserData() {
+        System.out.println("==회원정보 테스트 데이터 생성==");
+        members.add(new Member(1, Util.getNowStr(), Util.getNowStr(), "Test1", "test1", "테스트1"));
+        members.add(new Member(2, Util.getNowStr(), Util.getNowStr(), "Test2", "test2", "테스트2"));
+        members.add(new Member(3, Util.getNowStr(), Util.getNowStr(), "Test3", "test3", "테스트3"));
+    }
+
+
 }
 
 class Member {
