72022f4089ba27ca86cb3b02c1102364abede5fa
leeseonju12 <seonju220@gmail.com>
2025-12-10 10:32:45 +0900

    App 간소화, Controller 추가, 상속

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,7 +12,9 @@ public class App {
         ArticleController articleController = new ArticleController(sc);
 
         articleController.makeTestData();
-        memberController.makeTestUserData();
+        memberController.makeTestData();
+
+        Controller controller = null;
 
         while (true) {
             System.out.print("명령어 ) ");
@@ -25,24 +27,43 @@ public class App {
                 continue;
             }
 
-            if (cmd.equals("member join")) {
-                memberController.doJoin();
-            } else if (cmd.equals("login")) {
-                memberController.doLogin();
+            String[] cmdBits = cmd.split(" ");
+
+            String controllerName = cmdBits[0];
+
+            if (cmdBits.length == 1) {
+                System.out.println("명령어 확인 필요");
+                continue;
             }
-            else if (cmd.equals("article write")) {
-                articleController.doWrite();
-            } else if (cmd.startsWith("article list")) {
-                articleController.showList(cmd);
-            } else if (cmd.startsWith("article detail")) {
-                articleController.showDetail(cmd);
-            } else if (cmd.startsWith("article delete")) {
-                articleController.doDelete(cmd);
-            } else if (cmd.startsWith("article modify")) {
-                articleController.doModify(cmd);
+
+            String actionMethodName = cmdBits[1];
+
+            if (controllerName.equals("article")) {
+                controller = articleController;
+            } else if (controllerName.equals("member")) {
+                controller = memberController;
             } else {
-                System.out.println("사용할 수 없는 명령어입니다");
+                System.out.println("지원하지 않는 기능");
+                continue;
             }
+
+            controller.doAction(cmd, actionMethodName);
+
+//            if (cmd.equals("member join")) {
+//                memberController.doJoin();
+//            } else if (cmd.equals("article write")) {
+//               articleController.doWrite();
+//            } else if (cmd.startsWith("article list")) {
+//               articleController.showList(cmd);
+//            } else if (cmd.startsWith("article detail")) {
+//              articleController.showDetail(cmd);
+//            } else if (cmd.startsWith("article delete")) {
+//                articleController.doDelete(cmd);
+//            } else if (cmd.startsWith("article modify")) {
+//              articleController.doModify(cmd);
+//            } else {
+//                System.out.println("사용할 수 없는 명령어입니다");
+//            }
         }
         System.out.println("==프로그램 끝==");
         sc.close();
--- src/main/java/org/example/MemberController.java
+++ src/main/java/org/example/MemberController.java
@@ -4,18 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class MemberController {
-    Scanner sc;
+public class MemberController extends Controller {
 
-    int lastMemberId = 3;
-    List<Member> members;
+    private Scanner sc;
+    private List<Member> members;
+    private String cmd;
+
+    private int lastMemberId = 3;
 
     public MemberController(Scanner sc) {
         this.sc = sc;
         members = new ArrayList<>();
     }
 
-    public void doJoin() {
+    public void doAction(String cmd, String actionMethodName) {
+        this.cmd = cmd;
+
+        switch (actionMethodName) {
+            case "join":
+                doJoin();
+                break;
+            default:
+                System.out.println("Invalid action method");
+                break;
+        }
+    }
+
+    private void doJoin() {
         System.out.println("==회원 가입==");
         int id = lastMemberId + 1;
         String loginId = null;
@@ -52,26 +67,6 @@ public class MemberController {
         lastMemberId++;
     }
 
-    public void doLogin() {
-        System.out.print("아이디: ");
-        String loginId = sc.nextLine();
-        System.out.print("비밀번호: ");
-        String password = sc.nextLine();
-
-        boolean loginSuccess = false;
-        for (Member member : members) {
-            if (member.getLoginId().equals(loginId) && member.getPassword().equals(password)) { //현재 검사중인 user객체의 아이디와 비밀번호가 맞는 경우
-                System.out.println(member.getId() + "번 회원 로그인이 성공되었습니다.\n");
-                loginSuccess = true;
-                break;
-            }
-        }
-        if (!loginSuccess) {
-            // 로그인 실패 시 메시지를 한 번만 출력
-            System.out.println("로그인 실패. 아이디나 비밀번호를 다시 입력하세요.\n");
-        }
-    }
-
     private boolean isJoinableLoginId(String loginId) {
         for (Member member : members) {
             if (member.getLoginId().equals(loginId)) {
@@ -81,14 +76,13 @@ public class MemberController {
         return true;
     }
 
-
     /**
-     * 회원정보 테스트 데이터 생성
+     * 회원 테스트 데이터 생성
      **/
-    public void makeTestUserData() {
-        System.out.println("==회원정보 테스트 데이터 생성==");
-        members.add(new Member(1, Util.getNowStr(), Util.getNowStr(), "Test1", "test1", "테스트1"));
-        members.add(new Member(2, Util.getNowStr(), Util.getNowStr(), "Test2", "test2", "테스트2"));
-        members.add(new Member(3, Util.getNowStr(), Util.getNowStr(), "Test3", "test3", "테스트3"));
+    public void makeTestData() {
+        System.out.println("==회원 테스트 데이터 생성==");
+        members.add(new Member(1, Util.getNowStr(), Util.getNowStr(), "test1", "test1", "회원1"));
+        members.add(new Member(2, Util.getNowStr(), Util.getNowStr(), "test2", "test2", "회원2"));
+        members.add(new Member(3, Util.getNowStr(), Util.getNowStr(), "test3", "test3", "회원3"));
     }
 }
